c00305dda72597dfd43373b62486f5941a7f6c40
Stephen Keable <hello@stephenkeable.co.uk>
2018-03-20 15:25:41 +0000

    remove placename endpoints

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ Create instance and basic default options
 */
 
 var AlliesGeocoding = function () {
-    
+
     if (!(this instanceof AlliesGeocoding)) {
-        
+
         return new AlliesGeocoding();
     }
-    
+
     var config = {
         url_base: "https://ws.postcoder.com/pcw/",
         page_num: 0,
@@ -23,9 +23,9 @@ var AlliesGeocoding = function () {
             category: "places",
         }
     };
-    
+
     this.config = config;
-    
+
 }
 
 /*
@@ -42,37 +42,37 @@ Debug use true to check search key against status service and console.log the re
 */
 
 AlliesGeocoding.prototype.init = function (api_key, options, debug) {
-    
+
     if (typeof api_key === "string") {
         this.config.api_key = api_key;
     } else {
         this.config.api_key = "PCW45-12345-12345-1234X";
     }
-    
+
     var new_options = options || false;
-    
+
     if (new_options) {
         this.setOptions(new_options);
     }
-    
+
     this.config.debug = debug || false;
-    
+
     if (this.config.debug === true) {
-    
+
         this.checkStatus(function(response, error) {
 
             if(error) {
-                
-                console.log(error);   
-                
+
+                console.log(error);
+
             } else {
-            
+
                 console.log(response);
             }
         });
-        
+
     }
-    
+
 };
 
 /*
@@ -82,7 +82,7 @@ Overwrite the options object after init
 */
 
 AlliesGeocoding.prototype.setOptions = function (object) {
-   this.config.options = object; 
+   this.config.options = object;
 };
 
 /*
@@ -93,7 +93,7 @@ Overwrite the page_num
 
 AlliesGeocoding.prototype.setPageNum = function (page_num) {
     if(typeof page_num == "number") {
-        this.config.page_num = parseInt(page_num, 10); 
+        this.config.page_num = parseInt(page_num, 10);
     }
 };
 
@@ -104,15 +104,15 @@ Status endpoint give information about number of credits available amongst other
 */
 
 AlliesGeocoding.prototype.checkStatus = function (callback) {
-  
+
     var request_url = this.config.url_base + this.config.api_key + "/status";
-    
+
     send_request(request_url, function(result, error) {
 
         return callback(result, error);
-        
+
     });
-    
+
 };
 
 
@@ -123,15 +123,15 @@ Return the geodata for a given postcode
 */
 
 AlliesGeocoding.prototype.geoFromPostcode = function (search, callback) {
-  
+
     var request_url = this.config.url_base + this.config.api_key + "/position/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-    
+
     send_request(request_url, function(result, error) {
 
         return callback(result, error);
-        
+
     });
-    
+
 };
 
 
@@ -142,15 +142,15 @@ Return the street name(s) along with geodata for a given postcode
 */
 
 AlliesGeocoding.prototype.searchStreetGeo = function (search, callback) {
-  
+
     var request_url = this.config.url_base + this.config.api_key + "/geo/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-    
+
     send_request(request_url, function(result, error) {
 
         return callback(result, error);
-        
+
     });
-    
+
 };
 
 
@@ -161,13 +161,13 @@ Return the nearest streets for given latitude and longitude, within radius (metr
 */
 
 AlliesGeocoding.prototype.streetFromGeo = function (latitude, longitude, radius, callback) {
-    
+
     var radius = radius || 50;
     var latitude = latitude || false;
     var longitude = longitude || false;
-    
+
     if(trim_check(latitude) !== false && trim_check(longitude) !== false) {
-  
+
         var request_url = this.config.url_base + this.config.api_key + "/rgeo/UK/" + encodeURIComponent(latitude) + "/" + encodeURIComponent(longitude) + "?distance=" + encodeURIComponent(radius) + "&" + qs.stringify(this.config.options);
 
         send_request(request_url, function(result, error) {
@@ -175,17 +175,17 @@ AlliesGeocoding.prototype.streetFromGeo = function (latitude, longitude, radius,
             return callback(result, error);
 
         });
-        
+
     } else {
-        
+
         var error_response = {
             http_status: 404,
             error_body: "No latitude or/and longitude parameter supplied"
         };
-        
+
         return callback(false, error_response);
     }
-    
+
 };
 
 
@@ -196,11 +196,11 @@ Return geodata for a given IP address
 */
 
 AlliesGeocoding.prototype.geoFromIp = function (ip_address, callback) {
-    
+
     var ip_address = ip_address || false;
-    
+
     if(ip_address !== false && ip_address.trim() != "") {
-  
+
         var request_url = this.config.url_base + this.config.api_key + "/geolocate/ip/" + encodeURIComponent(ip_address) + "?" + qs.stringify(this.config.options);
 
         send_request(request_url, function(result, error) {
@@ -208,115 +208,62 @@ AlliesGeocoding.prototype.geoFromIp = function (ip_address, callback) {
             return callback(result, error);
 
         });
-        
+
     } else {
-        
+
         var error_response = {
             http_status: 404,
             error_body: "No IP Address parameter supplied"
         };
-        
-        return callback(false, error_response);
-        
-    }
-    
-};
 
+        return callback(false, error_response);
+
+    }
+
+};
 
 /*
-
-Search for place name and return geodata along with other information
-
-*/
-
-AlliesGeocoding.prototype.searchPlaceName = function (place_name, callback) {
-      
-    var request_url = this.config.url_base + this.config.api_key + "/placenames/GB/" + encodeURIComponent(place_name) + "?" + qs.stringify(this.config.options);
-
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-
-    });
-    
-};
-
-
-/*
-
-Return geodata and other info for a single Place Name using ID
-
-*/
-
-AlliesGeocoding.prototype.getSinglePlaceName = function (place_id, callback) {
-    
-    var place_id = place_id || false;
-    
-    if(trim_check(place_id) !== false) {
-      
-        var request_url = this.config.url_base + this.config.api_key + "/placenames/GB/id?id=" + encodeURIComponent(place_id) + "&" + qs.stringify(this.config.options);
-
-        send_request(request_url, function(result, error) {
-
-            return callback(result, error);
-
-        });
-        
-    } else {
-        
-        var error_response = {
-            http_status: 404,
-            error_body: "No Place ID parameter supplied"
-        };
-        
-        return callback(false, error_response);
-        
-    }
-    
-};
-
-/* 
 
 Internal helper functions
 
 */
 
 function send_request(request_url, callback) {
-    
+
     // Could probably get rid of dependency on the 'request' library to be honest
-    
+
     request(request_url, function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
 
             // Convert response into a JSON object
             var status_response = JSON.parse(body);
-            
+
             return callback(status_response, false);
-            
+
         } else {
             if (error) {
-            
+
                 return callback(false, error);
-                
+
             } else {
-                
+
                 var error_response = {
                     http_status: response.statusCode,
                     error_body: body
                 };
-            
+
                 return callback(false, error_response);
             }
 
         }
 
     });
-    
+
 }
 
 function trim_check(text) {
-    
+
     if(text === false) {
         return false;
     } else if (text.trim() == "") {
@@ -324,7 +271,7 @@ function trim_check(text) {
     } else {
         return true
     }
-    
+
 }
 
 module.exports = new AlliesGeocoding();
