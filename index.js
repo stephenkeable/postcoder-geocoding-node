9f6c138a60c0b2b33500f46629ca307d72afd384
Stephen Keable <hello@stephenkeable.co.uk>
2020-04-16 12:39:35 +0100

    move to es6 and 0 dependencies

--- index.js
+++ index.js
@@ -1,243 +1,162 @@
-"use strict";
-
-var request = require('request');
-var qs = require('qs');
-
+const https = require('https');
 /*
-
 Create instance and basic default options
-
 */
 
-var AlliesGeocoding = function () {
-
-    if (!(this instanceof AlliesGeocoding)) {
-
-        return new AlliesGeocoding();
-    }
-
-    var config = {
-        url_base: "https://ws.postcoder.com/pcw/",
-        page_num: 0,
-        options: {
-            category: "places",
-        }
+class AlliesGeocoding {
+  constructor() {
+    this.config = {
+      urlBase: 'https://ws.postcoder.com/pcw/',
+      pageNum: 0,
+      options: {
+        category: 'places',
+      },
     };
+  }
 
-    this.config = config;
+  /*
+  Init function
 
-}
+  Sets up API key (required, will default to Test key if not supplied)
 
-/*
+  Optional
 
-Init function
-
-Sets up API key (required, will default to Test key if not supplied)
-
-Optional
-
-Options object which is turned into query string parameters
-Debug use true to check search key against status service and console.log the result
-
-*/
-
-AlliesGeocoding.prototype.init = function (api_key, options, debug) {
-
-    if (typeof api_key === "string") {
-        this.config.api_key = api_key;
+  Options object which is turned into query string parameters
+  Debug use true to check search key against status service and console.log the result
+  */
+  init(apiKey, options, debug) {
+    if (typeof apiKey === 'string') {
+      this.config.apiKey = apiKey;
     } else {
-        this.config.api_key = "PCW45-12345-12345-1234X";
+      this.config.apiKey = 'PCW45-12345-12345-1234X';
     }
 
-    var new_options = options || false;
+    const newOptions = options || false;
 
-    if (new_options) {
-        this.setOptions(new_options);
+    if (newOptions) {
+      this.setOptions(newOptions);
     }
 
     this.config.debug = debug || false;
 
     if (this.config.debug === true) {
-
-        this.checkStatus(function(response, error) {
-
-            if(error) {
-
-                console.log(error);
-
-            } else {
-
-                console.log(response);
-            }
-        });
-
-    }
-
-};
-
-/*
-
-Overwrite the options object after init
-
-*/
-
-AlliesGeocoding.prototype.setOptions = function (object) {
-   this.config.options = object;
-};
-
-/*
-
-Overwrite the page_num
-
-*/
-
-AlliesGeocoding.prototype.setPageNum = function (page_num) {
-    if(typeof page_num == "number") {
-        this.config.page_num = parseInt(page_num, 10);
-    }
-};
-
-/*
-
-Status endpoint give information about number of credits available amongst others
-
-*/
-
-AlliesGeocoding.prototype.checkStatus = function (callback) {
-
-    var request_url = this.config.url_base + this.config.api_key + "/status";
-
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-
-    });
-
-};
-
-
-/*
-
-Return the geodata for a given postcode
-
-*/
-
-AlliesGeocoding.prototype.geoFromPostcode = function (search, callback) {
-
-    var request_url = this.config.url_base + this.config.api_key + "/position/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-
-    });
-
-};
-
-
-/*
-
-Return the street name(s) along with geodata for a given postcode
-
-*/
-
-AlliesGeocoding.prototype.searchStreetGeo = function (search, callback) {
-
-    var request_url = this.config.url_base + this.config.api_key + "/geo/UK/" + encodeURIComponent(search) + "?" + qs.stringify(this.config.options);
-
-    send_request(request_url, function(result, error) {
-
-        return callback(result, error);
-
-    });
-
-};
-
-
-/*
-
-Return the nearest streets for given latitude and longitude, within radius (metres)
-
-*/
-
-AlliesGeocoding.prototype.streetFromGeo = function (latitude, longitude, radius, callback) {
-
-    var radius = radius || 50;
-    var latitude = latitude || false;
-    var longitude = longitude || false;
-
-    if(trim_check(latitude) !== false && trim_check(longitude) !== false) {
-
-        var request_url = this.config.url_base + this.config.api_key + "/rgeo/UK/" + encodeURIComponent(latitude) + "/" + encodeURIComponent(longitude) + "?distance=" + encodeURIComponent(radius) + "&" + qs.stringify(this.config.options);
-
-        send_request(request_url, function(result, error) {
-
-            return callback(result, error);
-
-        });
-
-    } else {
-
-        var error_response = {
-            http_status: 404,
-            error_body: "No latitude or/and longitude parameter supplied"
-        };
-
-        return callback(false, error_response);
-    }
-
-};
-
-/*
-
-Internal helper functions
-
-*/
-
-function send_request(request_url, callback) {
-
-    // Could probably get rid of dependency on the 'request' library to be honest
-
-    request(request_url, function (error, response, body) {
-
-        if (!error && response.statusCode == 200) {
-
-            // Convert response into a JSON object
-            var status_response = JSON.parse(body);
-
-            return callback(status_response, false);
-
+      this.checkStatus((response, error) => {
+        if (error) {
+          console.log(error);
         } else {
-            if (error) {
-
-                return callback(false, error);
-
-            } else {
-
-                var error_response = {
-                    http_status: response.statusCode,
-                    error_body: body
-                };
-
-                return callback(false, error_response);
-            }
-
+          console.log(response);
         }
-
-    });
-
-}
-
-function trim_check(text) {
-
-    if(text === false) {
-        return false;
-    } else if (text.trim() == "") {
-        return false;
-    } else {
-        return true
+      });
     }
+  }
 
+  /*
+  Internal helper functions
+  */
+  static sendRequest(requestUrl, callback) {
+    https
+      .get(requestUrl, (response) => {
+        const data = [];
+        response.on('data', (chunk) => {
+          data.push(chunk);
+        });
+        response.on('end', () => {
+          if (response.statusCode === 200) {
+            const theResponse = JSON.parse(data.join());
+            return callback(theResponse, false);
+          }
+          const errorResponse = {
+            http_status: response.statusCode,
+            error_body: 'Error',
+          };
+          return callback(false, errorResponse);
+        });
+      })
+      .on('error', (e) => {
+        const errorResponse = {
+          http_status: 500,
+          error_body: e,
+        };
+        return callback(false, errorResponse);
+      });
+  }
+
+  static trimCheck(text) {
+    if (text === false) {
+      return false;
+    }
+    if (text.trim() === '') {
+      return false;
+    }
+    return true;
+  }
+
+  static queryString(object) {
+    return Object.keys(object)
+      .map((key) => `${key}=${object[key]}`)
+      .join('&');
+  }
+
+  /*
+  Overwrite the options object after init
+  */
+  setOptions(object) {
+    this.config.options = object;
+  }
+
+  /*
+  Overwrite the pageNum
+  */
+  setPageNum(pageNum) {
+    if (typeof pageNum === 'number') {
+      this.config.pageNum = parseInt(pageNum, 10);
+    }
+  }
+
+  /*
+  Status endpoint give information about number of credits available amongst others
+  */
+  checkStatus(callback) {
+    const requestUrl = `${this.config.urlBase}${this.config.apiKey}/status`;
+    AlliesGeocoding.sendRequest(requestUrl, (result, error) => callback(result, error));
+  }
+
+  /*
+  Return the geodata for a given postcode
+  */
+  geoFromPostcode(search, callback) {
+    const requestUrl = `${this.config.urlBase}${this.config.apiKey}/position/UK/${encodeURIComponent(search)}?${AlliesGeocoding.queryString(this.config.options)}`;
+    AlliesGeocoding.sendRequest(requestUrl, (result, error) => callback(result, error));
+  }
+
+  /*
+  Return the street name(s) along with geodata for a given postcode
+  */
+  searchStreetGeo(search, callback) {
+    const requestUrl = `${this.config.urlBase}${this.config.apiKey}/geo/UK/${encodeURIComponent(search)}?${AlliesGeocoding.queryString(this.config.options)}`;
+    AlliesGeocoding.sendRequest(requestUrl, (result, error) => callback(result, error));
+  }
+
+  /*
+  Return the nearest streets for given latitude and longitude, within radius (metres)
+  */
+  streetFromGeo(latitude, longitude, radius, callback) {
+    const theRadius = radius || 50;
+    const theLatitude = latitude || false;
+    const theLongitude = longitude || false;
+
+    if (AlliesGeocoding.trimCheck(latitude) !== false
+      && AlliesGeocoding.trimCheck(longitude) !== false) {
+      const requestUrl = `${this.config.urlBase}${this.config.apiKey}/rgeo/UK/${encodeURIComponent(theLatitude)}/${encodeURIComponent(theLongitude)}?distance=${encodeURIComponent(theRadius)}&${AlliesGeocoding.queryString(this.config.options)}`;
+      AlliesGeocoding.sendRequest(requestUrl, (result, error) => callback(result, error));
+      return true;
+    }
+    const errorResponse = {
+      http_status: 404,
+      error_body: 'No latitude or/and longitude parameter supplied',
+    };
+    return callback(false, errorResponse);
+  }
 }
 
 module.exports = new AlliesGeocoding();
--- test/index.js
+++ test/index.js
@@ -3,455 +3,293 @@
 var assert = require('assert');
 
 describe('AlliesGeocoding', function() {
-   
+
     describe('.init()',function() {
-        
+
         it("Should default to the test key, when no key passed", function() {
-       
+
             var geocoding = require("../");
             geocoding.init();
-            
-            assert.equal("PCW45-12345-12345-1234X", geocoding.config.api_key);
-            
+
+            assert.equal("PCW45-12345-12345-1234X", geocoding.config.apiKey);
+
         });
-        
+
         it("Should default to the test key, when non string passed", function() {
-       
+
             var geocoding = require("../");
             geocoding.init(3);
-            
-            assert.equal(geocoding.config.api_key, "PCW45-12345-12345-1234X");
-            
+
+            assert.equal(geocoding.config.apiKey, "PCW45-12345-12345-1234X");
+
         });
-        
+
         it("Should use the key passed when is string", function() {
-       
+
             var geocoding = require("../");
             geocoding.init("ABC45-56789-12345-1234X");
-            
-            assert.equal(geocoding.config.api_key, "ABC45-56789-12345-1234X");
-            
+
+            assert.equal(geocoding.config.apiKey, "ABC45-56789-12345-1234X");
+
         });
-        
+
         it("Should use the default this.config.options object", function() {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init("ABC45-56789-12345-1234X");
-            
+
             assert.equal(geocoding.config.options.category, "places");
-            
+
         });
-        
+
         it("Should overwrite this.config.options object when one supplied", function() {
-       
+
             var geocoding = require("../");
-            
+
             var options = {
                 category: "streets"
             }
-            
+
             geocoding.init("ABC45-56789-12345-1234X", options);
-            
+
             assert.equal(geocoding.config.options.category, "streets");
-            
+
         });
-        
-        it("Should console.log info about api_key being used when passing true in third parameter");
-        
+
+        it("Should console.log info about apiKey being used when passing true in third parameter");
+
     });
-    
+
     describe('.setOptions()',function() {
-       
+
         it("Should overwrite this.config.options object when one supplied", function() {
-       
+
             var geocoding = require("../");
-            
+
             var options = {
                 lines: 4,
                 addtags: "udprn"
             }
-            
+
             geocoding.init();
-            
+
             geocoding.setOptions(options);
-            
+
             assert.equal(geocoding.config.options.lines, 4);
             assert.equal(geocoding.config.options.addtags, "udprn");
-            
+
         });
-        
+
     });
-    
+
     describe('.setPageNum()',function() {
-       
-        it("Should NOT overwrite this.config.page_num value when non number supplied", function() {
-       
+
+        it("Should NOT overwrite this.config.pageNum value when non number supplied", function() {
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.setPageNum("garbage");
-            
-            assert.equal(geocoding.config.page_num, 0);
-            
+
+            assert.equal(geocoding.config.pageNum, 0);
+
         });
-       
-        it("Should overwrite this.config.page_num value when integer supplied", function() {
-       
+
+        it("Should overwrite this.config.pageNum value when integer supplied", function() {
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.setPageNum(1);
-            
-            assert.equal(geocoding.config.page_num, 1);
-            
+
+            assert.equal(geocoding.config.pageNum, 1);
+
         });
-       
-        it("Should overwrite this.config.page_num value when non integer number supplied (converts using parseint)", function() {
-       
+
+        it("Should overwrite this.config.pageNum value when non integer number supplied (converts using parseint)", function() {
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.setPageNum(1.3);
-            
-            assert.equal(geocoding.config.page_num, 1);
-            
+
+            assert.equal(geocoding.config.pageNum, 1);
+
         });
-       
-        it("Should overwrite this.config.page_num value when 0 supplied", function() {
-       
+
+        it("Should overwrite this.config.pageNum value when 0 supplied", function() {
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.setPageNum(0);
-            
-            assert.equal(geocoding.config.page_num, 0);
-            
+
+            assert.equal(geocoding.config.pageNum, 0);
+
         });
-        
+
     });
-    
+
     describe('.checkStatus()',function() {
-       
+
         it("Should return result object and false for error.", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.checkStatus( function(result, error) {
-            
+
                 assert.equal(typeof result, "object");
                 assert.equal(error, false);
-                
+
                 done();
-                
+
             });
-            
+
         });
-       
+
         it("Should fail when can't reach API, needs way of simulating broken connection");
-        
+
         it("Should inform you when using test key", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.checkStatus( function(result, error) {
-            
+
                 assert.equal(result.state, "Test Key");
                 assert.equal(result.correctsearchkey, true);
-                
+
                 done();
-                
+
             });
-            
+
         });
-       
+
         it("Should inform you when using incorrect key", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init("rsetghtdejgkfk");
-            
+
             geocoding.checkStatus( function(result, error) {
-            
+
                 assert.equal(result.state, "Incorrect Search Key");
                 assert.equal(result.correctsearchkey, false);
-                
+
                 done();
-                
+
             });
-            
+
         });
-        
+
     });
-    
+
     describe('.geoFromPostcode()',function() {
-        
+
         it("Should return result array with one record with latitude and longitude fields for NR14 7PZ", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.geoFromPostcode("NR14 7PZ", function(result, error) {
-            
+
                 assert.equal(Array.isArray(result), true);
                 assert.equal(result.length == 1, true);
                 assert.equal(result[0].latitude != 'undefined', true);
                 assert.equal(result[0].longitude != 'undefined', true);
-                
+
                 done();
-                
+
             });
-            
+
         });
-       
+
         it("Should return empty array with blank search parameter", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.geoFromPostcode("", function(result, error) {
-            
+
                 assert.equal(Array.isArray(result), true);
                 assert.equal(result.length, 0);
-                
+
                 done();
-                
+
             });
-            
+
         });
-        
+
     });
-    
+
     describe('.searchStreetGeo()',function() {
-        
+
         it("Should return result array with one record for NR14 7PZ", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.searchStreetGeo("NR14 7PZ", function(result, error) {
-            
+
                 assert.equal(Array.isArray(result), true);
                 assert.equal(result.length == 1, true);
-                
+
                 done();
-                
+
             });
-            
+
         });
-       
+
         it("Should return empty array with blank search parameter", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.searchStreetGeo("", function(result, error) {
-            
+
                 assert.equal(Array.isArray(result), true);
                 assert.equal(result.length, 0);
-                
+
                 done();
-                
+
             });
-            
+
         });
-       
+
         it("Should include latitude and longitude fields in first record", function(done) {
-       
+
             var geocoding = require("../");
-            
+
             geocoding.init();
-            
+
             geocoding.searchStreetGeo("NR14 7PZ", function(result, error) {
-            
+
                 assert.equal(Array.isArray(result), true);
                 assert.equal(result.length == 1, true);
                 assert.equal(result[0].latitude != 'undefined', true);
                 assert.equal(result[0].longitude != 'undefined', true);
-                
+
                 done();
-                
+
             });
-            
+
         });
     });
-    
+
     describe('.streetFromGeo()',function() {
-        
+
     });
-    
-    describe('.geoFromIp()',function() {
-        
-        it("Should return result object geodata fields for 82.71.8.24", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.geoFromIp("82.71.8.24", function(result, error) {
-            
-                assert.equal(typeof result, "object");
-                assert.equal(result.countryname, "United Kingdom");
-                assert.equal(result.iso2code, "GB");
-                assert.equal(result.latitude, "51.5");
-                assert.equal(result.longitude, "-0.13");
-                
-                done();
-                
-            });
-            
-        });
-       
-        it("Should return error.http_code=404 for blank IP address parameter", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.geoFromIp("", function(result, error) {
-            
-                assert.equal(error.http_status, 404);
-                
-                done();
-                
-            });
-            
-        });
-        
-    });
-    
-    describe('.searchPlaceName()',function() {
-        
-        it("Should return result array for Framingham Pigot", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.searchPlaceName("Framingham Pigot", function(result, error) {
-            
-                assert.equal(Array.isArray(result), true);
-                assert.equal(result.length > 1, true);
-                
-                done();
-                
-            });
-            
-        });
-       
-        it("Should return empty array with blank search parameter", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.searchPlaceName("", function(result, error) {
-            
-                assert.equal(Array.isArray(result), true);
-                assert.equal(result.length, 0);
-                
-                done();
-                
-            });
-            
-        });
-        
-        it("Should return result array with single record for Framingham Pigot and category=places", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            var options = {
-                category: "places"
-            };
-            
-            geocoding.setOptions(options);
-            
-            geocoding.searchPlaceName("Framingham Pigot", function(result, error) {
-            
-                assert.equal(Array.isArray(result), true);
-                assert.equal(result.length == 1, true);
-                
-                done();
-                
-            });
-            
-        });
-        
-        it("Should return result array for Framingham Pigot and category=streets, with first record name=Pigot Lane", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            var options = {
-                category: "streets"
-            };
-            
-            geocoding.setOptions(options);
-            
-            geocoding.searchPlaceName("Framingham Pigot", function(result, error) {
-            
-                assert.equal(Array.isArray(result), true);
-                assert.equal(result.length > 1, true);
-                assert.equal(result[0].name, "Pigot Lane");
-                
-                done();
-                
-            });
-            
-        });
-        
-    });
-    
-    describe('.getSinglePlaceName()',function() {
-        
-        it("Should return result array with single record for id=osgb4000000074572958", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.getSinglePlaceName("osgb4000000074572958", function(result, error) {
-            
-                assert.equal(Array.isArray(result), true);
-                assert.equal(result.length == 1, true);
-                
-                done();
-                
-            });
-            
-        });
-       
-        it("Should return error.http_code=404 for blank ID parameter", function(done) {
-       
-            var geocoding = require("../");
-            
-            geocoding.init();
-            
-            geocoding.getSinglePlaceName("", function(result, error) {
-            
-                assert.equal(error.http_status, 404);
-                
-                done();
-                
-            });
-            
-        });
-        
-    });
-    
+
 });
